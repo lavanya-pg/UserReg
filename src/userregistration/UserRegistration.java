6da21509ad4e23ee0b57ad11b0e419f89c9791b0
lavanya-pg <lavanyapg8@gmail.com>
2021-07-12 13:30:38 +0530

    ADDED: Code For Checking The EmailIds Are valid Or Not

--- src/userregistration/UserRegistration.java
+++ src/userregistration/UserRegistration.java
@@ -1,36 +1,52 @@
 package userregistration;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class UserRegistration
 {
-	Scanner scanner = new Scanner(System.in);
-    public static final String PASSWORDPATTERN = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*?&]{5,}$";
+    public static final String EMAILPATTERN = "([0-9A-Za-z])+([-+._][0-9A-Za-z]+)*" + "@([0-9A-Za-z])+[.]([a-zA-Z])+([.][A-Za-z]+)*";
+
+	 public static void main(String[] args)
+	    {
+
+	        UserRegistration userRegistration = new UserRegistration();
+	        userRegistration.emailpatternTest();
+	    }
+		private void emailpatternTest() // Email samples
+		{
+	        String sampleEmail1 = "abc@yahoo.com";
+	        String sampleEmail2 = "abc-100@yahoo.com";
+	        String sampleEmail3 = "abc.100@yahoo.com";
+	        String sampleEmail4 = "abc111@abc.com";
+	        String sampleEmail5 = "abc-100@abc.net";
+	        String sampleEmail6 = "abc.100@abc.com.au";
+	        String sampleEmail7 = "abc@1.com";
+	        String sampleEmail8 = "abc@gmail.com.com";
+	        String sampleEmail9 = "abc+100@gmail.com";
 
 
-    public static void main(String[] args)
-    {
-        UserRegistration userRegistration = new UserRegistration();
-        userRegistration.readInput();
-    }
-    private void readInput() 
-    {
-        System.out.println("Enter the PassWord ");
-        String PassWord = scanner.next();
-        validateInput(PASSWORDPATTERN,PassWord);
-    }
+	        ArrayList<String> emailIdList = new ArrayList<String>();
+	        emailIdList.add(sampleEmail1);
+	        emailIdList.add(sampleEmail2);
+	        emailIdList.add(sampleEmail3);
+	        emailIdList.add(sampleEmail4);
+	        emailIdList.add(sampleEmail5);
+	        emailIdList.add(sampleEmail6);
+	        emailIdList.add(sampleEmail7);
+	        emailIdList.add(sampleEmail8);
+	        emailIdList.add(sampleEmail9);
 
-    private void validateInput(String pattern, String input)
-    {
-
-        if(Pattern.matches(pattern, input))
-        {
-            System.out.println("Valid");
-        }
-        else
-        {
-            System.out.println("Invalid");
-        }
-    }
+	        for (String emailId : emailIdList)
+	        {
+	            if (Pattern.matches(EMAILPATTERN, emailId)) 
+	            {
+	                System.out.println(emailId + " - valid ");
+	            }
+	            else
+	            {
+	                System.out.println(emailId + " - Invalid ");
+	            }
+	        }
+	    }
 }
