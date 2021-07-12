d43c35c5ef28b315ad8f4ac0a9eb94c696baa498
lavanya-pg <lavanyapg8@gmail.com>
2021-07-12 12:54:44 +0530

    ADDED: Code for Enter The First Name

--- src/userregistration/UserRegistration.java
+++ src/userregistration/UserRegistration.java
@@ -1,9 +1,35 @@
 package userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration
 {
-	public static void main(String[] args)
-	{
-		System.out.println("Welcome To UserRegistration Program");
-	}
+	Scanner scanner = new Scanner(System.in);
+    public static final String NAMEPATTERN = "[A-Z][A-Za-z]{2,}";
+
+    public static void main(String[] args)
+    {
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.readInput();
+    }
+    private void readInput() //Method for reading an input.
+    {
+        System.out.println("Enter first name ");
+        String firstName = scanner.next();
+        validateInput(NAMEPATTERN,firstName);
+    }
+
+    private void validateInput(String pattern, String input)
+    {
+
+        if(Pattern.matches(pattern, input))
+        {
+            System.out.println("Valid");
+        }
+        else
+        {
+            System.out.println("Invalid");
+        }
+    }
 }
